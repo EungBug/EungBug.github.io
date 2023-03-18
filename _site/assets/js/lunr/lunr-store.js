d56df8fcefd9b101a9a2eb8e013688f24c835aab
EungBug <eungb14@gmail.com>
2023-03-19 01:41:06 +0900

    [⚙️] sitemap.xml delete

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "[Etc] Github(jekyll) 개발 블로그를 개설하다.",
-        "excerpt":"첫 포스트 테스트중..  ","categories": ["Etc"],
+        "excerpt":"first post test..  ","categories": ["Etc"],
         "tags": ["github page","jekyll"],
         "url": "/etc/Etc-BlogOpen/",
         "teaser": null
