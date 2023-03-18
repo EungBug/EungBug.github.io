3132b16de371460a7fb12182457bb11c9e51ec3c
EungBug <eungb14@gmail.com>
2023-03-19 01:57:20 +0900

    [🐞] sitemap error 수정중

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "[Etc] Github(jekyll) 개발 블로그를 개설하다.",
         "excerpt":"first post test..  ","categories": ["Etc"],
         "tags": ["github page","jekyll"],
-        "url": "/etc/Etc-BlogOpen/",
+        "url": "/etc/etc-01/",
         "teaser": null
       }]
