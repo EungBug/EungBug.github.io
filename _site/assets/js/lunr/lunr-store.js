5cb53ab757f1f2f2e38b70189d2857e03ca5cc77
EungBug <eungb14@gmail.com>
2023-03-19 00:52:22 +0900

    [⚙️] sitemap & robots 파일 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,1 @@
-var store = [{
-        "title": "chapter 1 첫 게시물 예시",
-        "excerpt":"위 형식을 참고하여 블로그를 커스텀하시오   - 게시물 업로드 양식   위 형식을 참고하여 블로그를 커스텀하시오   게시물 업로드 양식  ","categories": ["Blog"],
-        "tags": ["Blog","Git","Github"],
-        "url": "/blog/git-&-github/",
-        "teaser": null
-      }]
+var store = []
