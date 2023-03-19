9f824c7bf324214d0ff56621fab071ce2fdd6eb3
EungBug <eungb14@gmail.com>
2023-03-20 02:09:39 +0900

    [⚙️] sass color 수정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "[Etc] Github(jekyll) 개발 블로그를 개설하다.",
-        "excerpt":"first post test..  ","categories": ["Etc"],
+        "excerpt":"개발 블로그로 깃헙 블로그를 선택한 이유 기존에 티스토리로 블로그를 운영한 경험이 있다. 물론 운영이 쉽고 글을 바로 작성해서 포스팅하면 끝인 장점들이 있었지만 디자인적으로 한계가 있다는 느낌이 들었다. 이후 velog를 접하게 되었고 개발자들이 많이 쓰는 블로그라는 점에 있어서 매력을 느껴서 velog로 이사를 했었다. velog 또한 마크다운 문법을 통해 간단하게 바로 작성하고...","categories": ["Etc"],
         "tags": ["github page","jekyll"],
         "url": "/etc/etc-01/",
+        "teaser": null
+      },{
+        "title": "[Study] 마크다운(Markdown) 문법",
+        "excerpt":"패스트캠퍼스 박영웅 강사님 강의를 통해 학습한 내용을 기반으로 작성한 글입니다. 새로 배우는 문법들은 추후 추가적으로 업데이트 할 예정! 마크다운(Markdown) 마크다운(Markdown)은 일반 텍스트 기반의 경량 마크업 언어다. 일반 텍스트로 서식이 있는 문서를 작성하는 데 사용되며, 일반 마크업 언어에 비해 문법이 쉽고 간단한 것이 특징이다. (위키백과) 장점 문법이 쉽고 간결하다. 관리가 쉽다....","categories": ["Study"],
+        "tags": ["markdown"],
+        "url": "/study/study-01/",
         "teaser": null
       }]
