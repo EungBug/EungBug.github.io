ac7abc9f9c83ea1a5fdfc57cd4ba607520c2a6e4
EungBug <eungb14@gmail.com>
2023-03-21 16:20:45 +0900

    [📝]TIL-02 CSS개요까지 초안 작성

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,15 +5,21 @@ var store = [{
         "url": "/etc/etc-01/",
         "teaser": null
       },{
-        "title": "TIL-01 : HTML",
-        "excerpt":"HTML(Hyper Text Markup Language) Hyper Text : 참조를 통해 현재 문서에서 다른 문서로 즉시 접근할 수 있는 텍스트(링크) HTML은 웹의 구조를 담당한다. HTML의 기본 구성 요소 태그 태그란 다양한 구성 요소(텍스트, 이미지, 버튼 등)를 정의하는 역할을 하고 HTML 문법을 이루는 가장 작은 단위로 &lt;&gt;사이에 태그명을 넣는 형식이다. 속성 속성은 태그에...","categories": ["Study"],
-        "tags": ["HTML"],
-        "url": "/study/til-01-draft/",
-        "teaser": null
-      },{
         "title": "[Study] 마크다운(Markdown) 문법",
         "excerpt":"패스트캠퍼스 박영웅 강사님 강의를 통해 학습한 내용을 기반으로 작성한 글입니다. 새로 배우는 문법들은 추후 추가적으로 업데이트 할 예정! 마크다운(Markdown) 마크다운(Markdown)은 일반 텍스트 기반의 경량 마크업 언어다. 일반 텍스트로 서식이 있는 문서를 작성하는 데 사용되며, 일반 마크업 언어에 비해 문법이 쉽고 간단한 것이 특징이다. (위키백과) 장점 문법이 쉽고 간결하다. 관리가 쉽다....","categories": ["Study"],
         "tags": ["markdown"],
         "url": "/study/study-01/",
+        "teaser": null
+      },{
+        "title": "TIL-01 : HTML",
+        "excerpt":"HTML(Hyper Text Markup Language) Hyper Text : 참조를 통해 현재 문서에서 다른 문서로 즉시 접근할 수 있는 텍스트(링크) HTML은 웹의 구조를 담당한다. HTML의 기본 구성 요소 태그 태그란 다양한 구성 요소(텍스트, 이미지, 버튼 등)를 정의하는 역할을 하고 HTML 문법을 이루는 가장 작은 단위로 &lt;&gt;사이에 태그명을 넣는 형식이다. 속성 속성은 태그에...","categories": ["Study"],
+        "tags": ["HTML"],
+        "url": "/study/til-01/",
+        "teaser": null
+      },{
+        "title": "TIL-02 : CSS",
+        "excerpt":"CSS(Cascading Style Sheets) css 문법은 선택자와 선언부로 구분된다. 선택자 {속성: 값;} 선택자 : CSS 스타일을 적용할 대상(요소)를 지정하는 영역이다. 선언부 : 선택자에서 선택한 대상에 적용할 스타일을 작성하는 영역으로 선언부에는 반드시 속성과 값을 한 쌍으로 작성해야 한다. 값 뒤에는 세미콜론(;)을 넣어 스타일을 여러개 작성이 가능하다. 속성 : 스타일의 종류 값 :...","categories": ["Study"],
+        "tags": ["CSS"],
+        "url": "/study/til-02/",
         "teaser": null
       }]
